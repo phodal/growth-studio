c2eba8259d17c8e15d45e7c0f0cf817f1cdb7e9d
Fengda HUANG <h@phodal.com>
2016-10-20 17:50:33 +0800

    try to add funciton test

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, render_to_response, get_object_or_404
+from django.shortcuts import render_to_response
 
 def index(request):
     return render_to_response('index.html')
