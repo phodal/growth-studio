aa933d0fcfd47ac624943a03417f2e8aca3e3586
Fengda HUANG <h@phodal.com>
2016-10-20 09:25:50 +0800

    [ch4] build basic

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric.api import local
+from fabric.decorators import task
+
+@task
+def runserver():
+    local("./manage.py runserver 0.0.0.0:8000")
+
+
+@task
+def install(env="env"):
+    local("pip install -r requirements/%s.txt" % env)
+
+
+@task
+def test():
+    local("./manage.py test")
