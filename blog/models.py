47bf00039e41fcf3a2f00bcc972d52f7a7eed0ed
Fengda HUANG <h@phodal.com>
2016-11-11 17:16:46 +0800

    link users

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import permalink
 from django_markdown.models import MarkdownField
+from django.template.defaultfilters import slugify
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=100, unique=True)
-    author = models.CharField(max_length=100, unique=True)
+    author = models.ForeignKey(User)
     slug = models.SlugField(max_length=100, unique=True)
     content = MarkdownField(default='SOME STRING')
     posted = models.DateField(db_index=True, auto_now_add=True)
@@ -15,4 +17,4 @@ class Blog(models.Model):
 
     @permalink
     def get_absolute_url(self):
-        return 'view_blog_post', None, {'slug': self.slug}
+        return 'view_blog_post', None, {'slug': slugify(self.slug)}
