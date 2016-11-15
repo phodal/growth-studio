5e521794635fcd9fb3bbe2afcb56c731dd1cc7b4
Fengda HUANG <h@phodal.com>
2016-11-15 10:09:43 +0800

    add pageintaion for list page

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render_to_response, get_object_or_404
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from blog.models import Blog
 
 
 def blog_list(request):
+    blogs = Blog.objects.all()
+    paginator = Paginator(blogs, 5)
+
+    page = request.GET.get('page')
+    try:
+        blogs = paginator.page(page)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+    except EmptyPage:
+        blogs = paginator.page(paginator.num_pages)
+
     return render_to_response('blog/list.html', {
-        'blogs': Blog.objects.all()
+        'blogs': blogs
     })
 
 
