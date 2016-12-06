baa35b57b802815a7976e44a8bb776c98a8d5312
Fengda HUANG <h@phodal.com>
2016-12-06 10:16:00 +0800

    change pageintaion size

--- blog/api.py
+++ blog/api.py
@@ -34,7 +34,7 @@ class BlogSerializer(serializers.ModelSerializer):
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 10
 
